17cf49b8172a7240d71b08f15346952cefe5374c
henrybarklam <72357080+henrybarklam@users.noreply.github.com>
2021-12-04 17:01:29 -0500

    Henry's Commit
    Should resolve the conflicts

---
+++ login-page.js
@@ -0,0 +1,54 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (bclick) => {
+
+    //When this is done for the creation of user to send to the database, don't prevent default
+    //Otherwise it'll prevent the submission of the data
+    bclick.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+
+    //Will have to make the field connect to the back end database to check if the
+    //username and password match
+    if (username === "user" && password === "user"){
+        alert("Login successful");
+        location.href="index.html";
+        // location.reload();
+    } 
+    else {
+        loginErrorMsg.style.opacity = 5;
+    }
+
+})
+
+function toggleSidebar(){
+    document.getElementById("sidebar").classList.toggle('active');
+}
+
+function toggleTab(){
+    document.getElementById("tabs").classList.toggle('active');
+}
+
+function openTab(evt, tabName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tabs is-medium is-centered" );
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
